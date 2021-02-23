3d7ae5f8f37bd5fbda7dd47bb3ceb395d2fb979d
Roman <steammushroom@gmail.com>
2021-02-23 13:10:25 +0300

    Бенджамин Баттон

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box, Flex, IconButton } from "@chakra-ui/react";
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
+import { CloseIcon } from '@chakra-ui/icons'
+import { TiThMenu } from "react-icons/ti";
 import Logo from "./Logo";
 import MenuItem from "./MenuItem"
 
@@ -32,28 +33,6 @@ const routes = [
     }
 ]
 
-const CloseIconKek = () => (
-    <svg width="24" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
-        <title>Close</title>
-        <path
-            fill="white"
-            d="M9.00023 7.58599L13.9502 2.63599L15.3642 4.04999L10.4142 8.99999L15.3642 13.95L13.9502 15.364L9.00023 10.414L4.05023 15.364L2.63623 13.95L7.58623 8.99999L2.63623 4.04999L4.05023 2.63599L9.00023 7.58599Z"
-        />
-    </svg>
-);
-
-const MenuIcon = () => (
-    <svg
-        width="24px"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-        fill="white"
-    >
-        <title>Menu</title>
-        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-    </svg>
-);
-
 const Header = (props) => {
     const [show, setShow] = React.useState(false);
     const toggleMenu = () => setShow(!show);
@@ -76,7 +55,7 @@ const Header = (props) => {
             >
                 <Logo
                     w="100px"
-                    color={["white", "white", "primary.800", "primary.800"]}
+                    color={["white", "white", "teal.800", "teal.800"]}
                 />
             </Flex>
             <Box
@@ -94,7 +73,7 @@ const Header = (props) => {
                         />
                         :
                         <IconButton
-                            icon={<HamburgerIcon />}
+                            icon={<TiThMenu />}
                             colorScheme="teal"
                             bgColor="teal.700"
                             size="lg"
